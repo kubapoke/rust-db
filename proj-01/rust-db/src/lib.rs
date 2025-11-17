180e910485c9367840beb33b3eaafa92a09f73cb
kubapoke <q.paczka@gmail.com>
2025-11-17 01:34:43 +0100

    init rust-db

---
+++ proj-01/rust-db/src/lib.rs
@@ -0,0 +1,25 @@
+﻿use std::collections::HashMap;
+
+trait DatabaseKey: Eq {}
+
+impl<T: Eq> DatabaseKey for T {}
+
+enum Value {
+    Bool(bool),
+    String(String),
+    Int(i64),
+    Float(f64),
+}
+
+enum AnyDatabase {
+    StringDatabase(Database<String>),
+    IntDatabase(Database<i64>),
+}
+
+struct Record {
+    values: HashMap<String, Value>,
+}
+
+struct Database<K: DatabaseKey> {
+    records: HashMap<K, Record>,
+}
