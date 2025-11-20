3ef1d30d214dc9459665af60c725409aa07f27ea
kubapoke <q.paczka@gmail.com>
2025-11-21 00:57:12 +0100

    changed the file structure and implemented basic database framework

---
+++ proj-01/rust-db/src/db/database.rs
@@ -0,0 +1,39 @@
+﻿use std::collections::HashMap;
+
+trait DatabaseKey: Eq + Ord {}
+
+impl DatabaseKey for String {}
+impl DatabaseKey for i64 {}
+
+enum FieldType {
+    Bool,
+    String,
+    Int,
+    Float,
+}
+
+enum Value {
+    Bool(bool),
+    String(String),
+    Int(i64),
+    Float(f64),
+}
+
+struct Record {
+    values: HashMap<String, Value>,
+}
+
+struct Table<K: DatabaseKey> {
+    key: String,
+    fields: HashMap<String, FieldType>,
+    records: HashMap<K, Record>,
+}
+
+struct Database<K: DatabaseKey> {
+    tables: HashMap<String, Table<K>>,
+}
+
+enum AnyDatabase {
+    StringDatabase(Database<String>),
+    IntDatabase(Database<i64>),
+}
