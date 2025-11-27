7ed569a64d91a8ce75667052e8ae1ecff377e519
kubapoke <q.paczka@gmail.com>
2025-11-27 22:33:10 +0100

    implemented final where clause

--- proj-01/rust-db/src/commands/clauses/where.rs
+++ proj-01/rust-db/src/commands/clauses/where.rs
@@ -1,5 +1,5 @@
 use crate::commands::clauses::clause::Clause;
-use crate::commands::clauses::evaluable::AnyEvaluable;
+use crate::commands::clauses::evaluable::{AnyEvaluable, Evaluable};
 use crate::database::slice::TableSlice;
 use crate::errors::Error;
 
@@ -7,8 +7,22 @@ pub struct WhereClause<'a> {
     pub evaluation: AnyEvaluable<'a>,
 }
 
+impl WhereClause<'_> {
+    pub fn new(evaluation: AnyEvaluable) -> WhereClause {
+        WhereClause { evaluation }
+    }
+}
+
 impl Clause for WhereClause<'_> {
     fn apply(&self, slice: TableSlice) -> Result<TableSlice, Error> {
-        todo!()
+        let mut filtered = Vec::new();
+
+        for record in slice.records.into_iter() {
+            if self.evaluation.evaluate(&record)? {
+                filtered.push(record);
+            }
+        }
+
+        Ok(TableSlice::new(filtered))
     }
 }
